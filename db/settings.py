54273a410ee565864538dc1a2badea5b29cbca6a
Denis Parshin <fersawas@yandex.ru>
2025-01-24 22:46:48 +0400

    stg chg

--- db/settings.py
+++ db/settings.py
@@ -13,7 +13,9 @@ class Settings(BaseSettings):
     def get_db_url(self):
         try:
             if int(os.environ.get('AMVERA', 0)) == 1:
-                return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}/{self.DB_NAME}"
+                print('amvera')
+                return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@amvera-bebabobins-cnpg-imayimeidb-rw/{self.DB_NAME}"
+            print('local')
             return f"postgresql+asyncpg://{self.DB_USER}:{self.DB_PASSWORD}@{self.DB_HOST}:{self.DB_PORT}/{self.DB_NAME}"
         except Exception as e:
             logging.error('ERROR WHILE URL FROM TO DB', e)
